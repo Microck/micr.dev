be0fd2d0f95dad0524cd09f69088ed46e90e7c63
Microck <45483921+Microck@users.noreply.github.com>
2025-06-28 17:31:56 +0200

    im going to
    kms

--- microkeebs/vite.config.ts
+++ microkeebs/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // This is mandatory. It tells the app to look for assets at /microkeebs/assets/...
   base: "/microkeebs/",
   plugins: [react()],
   optimizeDeps: {
