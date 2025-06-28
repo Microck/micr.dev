d6bf58844be72c0e2da51cb0bbccfa7c19b2c752
Microck <45483921+Microck@users.noreply.github.com>
2025-06-28 17:03:33 +0200

    Update vite.config.ts

--- microkeebs/vite.config.ts
+++ microkeebs/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/microkeebs/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
