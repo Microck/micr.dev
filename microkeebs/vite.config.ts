851ec05d02aff6f1e615900966e1ee2ad9b46e96
Microck <45483921+Microck@users.noreply.github.com>
2025-06-28 16:37:30 +0200

    sdf

--- microkeebs/vite.config.ts
+++ microkeebs/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // NO 'base' property here!
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
