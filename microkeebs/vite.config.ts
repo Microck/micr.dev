1435118c620dc46e6edb75b7243a0b1688fd4274
Microck <45483921+Microck@users.noreply.github.com>
2025-06-28 17:53:20 +0200

    zxc
    zx

--- microkeebs/vite.config.ts
+++ microkeebs/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // This is mandatory. It tells the app to look for assets at /microkeebs/assets/...
+  // This MUST be set to the subfolder path.
   base: "/microkeebs/",
   plugins: [react()],
   optimizeDeps: {
